68f1c6294e8227d7e1e7c2ac4188a2073d636c98
ryan2135 <ryan2135@pacificu.edu>
2023-05-04 15:29:02 -0700

    Editing CSS in first JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello!');
+const para = document.querySelector("p");
+
+para.addEventListener("click", updateName);
+
+function updateName() {
+  const name = prompt("Enter a new name");
+  para.textContent = `Player 1: ${name}`;
+}
