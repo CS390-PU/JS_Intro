5ac3b0ca0ef6228cc56496b80118325af19e1fbb
ryan2135 <ryan2135@pacificu.edu>
2023-05-11 14:02:35 -0700

    Adding more obj creation & this

--- js/objects.js
+++ js/objects.js
@@ -7,3 +7,28 @@ const obj1 = {
 console.log (obj1.property1);
 console.log (obj1[2]);
 console.log (obj1["property 3"]);
+
+let cond = true;
+let x;
+if (cond) {
+  x = { greeting: "hi there" };
+}
+console.log (x.greeting);
+
+const myHonda = {
+  color: "red",
+  wheels: 4,
+  engine: { cylinders: 4, size: 2.2 },
+};
+console.log (myHonda.engine.cylinders);
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  return year;
+}
+const myCar = new Car("Eagle", "Talon TSi", 1993);
+console.log (myCar.make);
+console.log (Car ("1", "2", 3), make);
+console.log (make);
