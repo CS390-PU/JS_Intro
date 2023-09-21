55f1fbf0087616d0d62e2d6d68ccb90a0eeecd4c
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2023-09-21 17:08:40 +0000

    Clarified output

--- js/primitive_types.js
+++ js/primitive_types.js
@@ -10,17 +10,20 @@
 
 // 1. Number
 let anInt = 2, aDecimal = 1.5;
+console.log("Number Output");
 console.log(typeof anInt === "number");
 console.log(typeof aDecimal === "number");
 console.log(typeof anInt === "object");
 
 // 2. String
 let aStr = "hi";
+console.log("String Output");
 console.log(typeof aStr === "string");
 console.log(typeof aStr === "object");
 
 // 3. BigInt
 let aBigInt1 = BigInt(123456789012345), aBigInt2 = 1n;
+console.log("BigInt Output");
 console.log(typeof aBigInt1 === "bigint");
 console.log(typeof aBigInt1 === "object");
 console.log(typeof aBigInt2 === "bigint");
@@ -28,21 +31,25 @@ console.log(typeof aBigInt2 === "object");
 
 // 4. Boolean
 let aBool = true;
+console.log("Boolean Output");
 console.log(typeof aBool === "boolean");
 console.log(typeof aBool === "object");
 
 // 5. undefined
 var anUndefined;
+console.log("undefined Output");
 console.log(typeof anUndefined === "undefined");
 console.log(typeof aBanUndefinedool === "object");
 
 // 6. Symbol - don't ask right now
 let aSymbol = Symbol("Foo");
+console.log("Symbol Output");
 console.log(typeof aSymbol === "symbol");
 console.log(typeof aSymbol === "object");
 
 // 7. null
 let aNull = null;
+console.log("null Output");
 console.log(typeof null === "object");
 console.log(null === "object"); // false why?
 
